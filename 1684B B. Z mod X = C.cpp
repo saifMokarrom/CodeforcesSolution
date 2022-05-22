f57e883fdd5a1c0df431ba10fdc3d27f585934af
saifMokarrom <46572647+saifMokarrom@users.noreply.github.com>
2022-05-22 18:16:06 +0600

    Add files via upload

---
+++ 1684B B. Z mod X = C.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using  namespace  std;
+
+typedef long long ll;
+
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--){
+        ll a,b,c;
+        cin>>a>>b>>c;
+        cout<<c+b+a<<" "<<c+b<<" "<<c<<"\n";
+    }
+
+}
